8fd27ae84065bfb7cf5dbf727d8bb78059ab3251
MathieuZani <49155404+MathieuZani@users.noreply.github.com>
2020-11-29 12:40:24 +0100

    ajout component et services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SignoutComponent } from './auth/signout/signout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SigninComponent,
+    SignupComponent,
+    SignoutComponent
   ],
   imports: [
     BrowserModule,
